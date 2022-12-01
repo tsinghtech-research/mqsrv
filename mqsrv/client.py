33477b298d21c826ab62b5000771944a1d65badd
fengxx <283570872@qq.com>
2022-12-01 17:07:53 +0800

    fix unknown delivery tag

--- mqsrv/client.py
+++ mqsrv/client.py
@@ -76,6 +76,8 @@ class MessageQueueClient:
             self.req_events[req_id].set((req_id, error, result))
 
     def run(self):
+        # Fix amqp.exceptions.PreconditionFailed: Basic.reject: (406) PRECONDITION_FAILED - unknown delivery tag 1
+        time.sleep(0.001)
         with Consumer(self.conn,
                       on_message=self.on_response,
                       queues=[self.callback_queue],
